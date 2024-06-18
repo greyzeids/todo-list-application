6ca1bc02453e2df69364e3993b5ad90c223a42d7
Miquel Carnot <mcgraphicss@gmail.com>
2024-06-19 00:49:15 +0200

    More changes - Style improved. - Added checkbox.

--- src/js/component/TodoList.jsx
+++ src/js/component/TodoList.jsx
@@ -15,44 +15,82 @@ export const TodoList = () => {
         localStorage.setItem("todos", JSON.stringify(todos));
     }, [todos]);
 
+    const handleDelete = (indexToDelete) => {
+        const updatedTodos = todos.filter(
+            (_, index) => index !== indexToDelete
+        );
+        setTodos(updatedTodos);
+    };
+
+    const handleAddTodo = () => {
+        if (inputValue.trim() !== "") {
+            const newTodo = { text: inputValue, completed: false };
+            setTodos([...todos, newTodo]);
+            setInputValue("");
+        }
+    };
+
+    const handleToggleCompleted = (indexToToggle) => {
+        const updatedTodos = todos.map((todo, index) => {
+            if (index === indexToToggle) {
+                return { ...todo, completed: !todo.completed };
+            }
+            return todo;
+        });
+        setTodos(updatedTodos);
+    };
+
     return (
-        <div className="container mt-5">
-            <h1 className="text-center text-success">Lista de la compra</h1>
-            <ul className="row p-0">
-                <li className="border-0 p-0">
-                    <input
-                        onChange={(e) => setInputValue(e.target.value)}
-                        value={inputValue}
-                        onKeyDown={(e) => {
-                            if (e.key === "Enter" && inputValue.trim() !== "") {
-                                setTodos(todos.concat(inputValue));
-                                setInputValue("");
-                            }
-                        }}
-                        name="text"
-                        placeholder="Me falta..."
-                        className="input"
-                        type="text"
-                    />
-                </li>
-                {todos.map((item, index) => (
-                    <li key={index}>
-                        {item}{" "}
+        <div className="container">
+            <h1 className="text-center">Lista de la compra</h1>
+            <div className="col-12">
+                <input
+                    onChange={(e) => setInputValue(e.target.value)}
+                    value={inputValue}
+                    onKeyDown={(e) => {
+                        if (e.key === "Enter" && inputValue.trim() !== "") {
+                            handleAddTodo();
+                        }
+                    }}
+                    name="text"
+                    placeholder="¿Cuál es la tarea hoy?"
+                    className="input"
+                    type="text"
+                />
+                <button
+                    type="submit"
+                    className="todo-btn"
+                    onClick={handleAddTodo}
+                >
+                    Agregar Tarea
+                </button>
+            </div>
+            <ul className="col-12 p-0 mt-3">
+                {todos.map((todo, index) => (
+                    <li
+                        key={index}
+                        className={`Todo text-white ${
+                            todo.completed ? "completed" : ""
+                        }`}
+                    >
+                        <input
+                            type="checkbox"
+                            checked={todo.completed}
+                            onChange={() => handleToggleCompleted(index)}
+                            className="completed mr-2"
+                        />
+                        {todo.text}
                         <i
-                            className="icono mt-1 fa fa-solid fa-delete-left"
-                            onClick={() =>
-                                setTodos(
-                                    todos.filter(
-                                        (_, currentIndex) =>
-                                            index !== currentIndex
-                                    )
-                                )
-                            }
+                            className="ml-2 fa fa-trash"
+                            style={{ cursor: "pointer" }}
+                            onClick={() => handleDelete(index)}
                         ></i>
                     </li>
                 ))}
             </ul>
-            <div className="p-0 text-white">{todos.length} elemento(s)</div>
+            <div className="col-12 p-0 text-white text-center">
+                {todos.length} elemento(s)
+            </div>
         </div>
     );
 };
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -2,7 +2,11 @@ import React from "react";
 import TodoList from "./TodoList";
 
 export const Home = () => {
-    return <TodoList />;
+    return (
+        <div className="FormBack">
+            <TodoList />
+        </div>
+    );
 };
 
 export default Home;
